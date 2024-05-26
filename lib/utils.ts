ca293ab00258e92f0d2e9f92290c2812bfeb3456
Ebuka Ezenwabasili <wizzyblaq94@gmail.com>
2024-05-26 21:46:34 +0100

    feature: added tw class name merge function

---
+++ lib/utils.ts
@@ -0,0 +1,16 @@
+import { type ClassValue, clsx } from "clsx";
+import qs from "query-string";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export function formatAmount(amount: number): string {
+  const formatter = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  });
+  return formatter.format(amount);
+}
