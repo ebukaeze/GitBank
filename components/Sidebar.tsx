40ec169af0ad0276ede4d4ba070aa6bf478ba850
Ebuka Ezenwabasili <wizzyblaq94@gmail.com>
2024-06-07 16:57:51 +0100

    chore: added footer section for user info and logout func

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 import React from "react";
 import { cn } from "@/lib/utils";
 import { usePathname } from "next/navigation";
+import Footer from "./Footer";
 
 const Sidebar = ({ user }: SiderbarProps) => {
   const pathname = usePathname();
@@ -59,7 +60,7 @@ const Sidebar = ({ user }: SiderbarProps) => {
         })}
         USER
       </nav>
-      FOOTER
+      <Footer user={user} />
     </section>
   );
 };
